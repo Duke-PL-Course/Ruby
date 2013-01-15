e6e249c263ad9b1aeb9bb6cdb59a9d45460bfbf0
Kevin Gao <sudowork@gmail.com>
2013-01-15 16:17:25 -0500

    Reordered examples

--- examples/2013-01-15-lecture01.rb
+++ examples/2013-01-15-lecture01.rb
@@ -8,18 +8,6 @@
 # Let's prove that these are objects
 42.methods    # shows the methods available on the object
 
-# An interesting thing about arrays is they can contain mixed types
-[1, "two", 3] # is perfectly valid
-Array.new     # remember, these are objects, and objects can be instantiated
-              #   with .new()
-
-# Alright, how about defining variables and calling functions?
-foo = 'hello, plcourse' # here we define a function. Notice there's no type declaration
-puts(foo)               # this prints "hello, plcourse" and returns nil
-puts foo                # this is exactly the same as above, with a bit of syntactic sugar
-foo = 'is mutable'      # variables are mutable, i.e. they can vary
-CONSTANTS = 'are defined like this' # they are immutable
-
 # In addition to being object-oriented, Ruby is strongly typed
 4 + 4       # => 8
 4 + "foo"   # results in a TypeError
@@ -39,3 +27,15 @@ while i < a.size
   puts a[i].to_i
   i = i + 1
 end
+
+# Alright, how about defining variables and calling functions?
+foo = 'hello, plcourse' # here we define a function. Notice there's no type declaration
+puts(foo)               # this prints "hello, plcourse" and returns nil
+puts foo                # this is exactly the same as above, with a bit of syntactic sugar
+foo = 'is mutable'      # variables are mutable, i.e. they can vary
+CONSTANTS = 'are defined like this' # they are immutable
+
+# An interesting thing about arrays is they can contain mixed types
+[1, "two", 3] # is perfectly valid
+Array.new     # remember, these are objects, and objects can be instantiated
+              #   with .new()

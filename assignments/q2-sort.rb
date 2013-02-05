d93405c1b080bdc26a8f2b5d627357b966cf4550
Kevin Gao <sudowork@gmail.com>
2013-02-04 19:08:49 -0500

    Added q2 as merge sort

--- assignments/q2-sort.rb
+++ assignments/q2-sort.rb
@@ -1,3 +1,19 @@
 def sort(arr)
-  # ...
+  # base case
+  n = arr.length
+  return arr if (n <= 1)
+
+  # partition step
+  left, right = sort(arr[0...n/2]), sort(arr[n/2...n])
+
+  # merge step
+  sorted = []
+  while left.size > 0 || right.size > 0
+    if right.size == 0 || (!left[0].nil? && left[0] < right[0])
+      sorted.push left.shift
+    else
+      sorted.push right.shift
+    end
+  end
+  sorted
 end

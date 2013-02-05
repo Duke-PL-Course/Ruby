cb137205d94bf6276d8a9da469b7114ac4666264
Kevin Gao <sudowork@gmail.com>
2013-02-04 19:23:40 -0500

    Added csv

--- assignments/q4-csv.rb
+++ assignments/q4-csv.rb
@@ -1,3 +1,10 @@
+# Alternative 1 using method_missing on Hash
+class Hash
+  def method_missing name, *args
+    self[name.to_s]
+  end
+end
+
 module ActsAsCsv
   def self.included(base)
     base.extend ClassMethods
@@ -10,11 +17,26 @@ module ActsAsCsv
   end
 
   module InstanceMethods
+    def read
+      @csv_contents = []
+      filename = self.class.to_s.downcase + '.txt'
+      file = File.new(filename)
+      @headers = file.gets.chomp.split(', ')
+
+      file.each do |row|
+        @csv_contents << row.chomp.split(', ')
+      end
+    end
+
+    def each
+      @csv_contents.each do |row|
+        yield Hash[@headers.zip row]
+      end
+    end
+
     attr_accessor :headers, :csv_contents
-    def initialize(rows)
-      first, *rest = rows
-      @headers = first.split(', ')
-      @csv_contents = rest.map { |row| row.split(', ') }
+    def initialize
+      read
     end
   end
 end
